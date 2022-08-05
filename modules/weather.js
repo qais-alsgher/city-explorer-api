1da92b399771799f743b9f1a685faa434a0d969c
qais-alsgher <qaisalsgher@gmail.com>
2022-08-05 21:31:07 +0300

    cache memory

--- modules/weather.js
+++ modules/weather.js
@@ -1,19 +1,30 @@
 const axios =require('axios');
+
+const weatherCache={};
+
 async function handleWeather(req,res){
     let lat =req.query.lat;
     let lon=req.query.lon;
-    
+    let searchQuery=req.query.searchQuery;
     // const city = weatherData.find(ele=> ele.city_name.toLowerCase() === searchQuery.toLowerCase());
-    const city =await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
-    // console.log(city.data);
-    try{
-    const weatherArr = city.data.data.map(items =>new Forecast(items));
-    // console.log(weatherArr);
-    res.send(weatherArr);
-    }catch(error){
-    
-    handleError(error,res);
+
+    if(weatherCache[searchQuery]!==undefined){
+
+      res.status(200).send(weatherCache[searchQuery]);
+
+    }else{
+      const city =await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
+      // console.log(city.data);
+      try{
+      const weatherArr = city.data.data.map(items =>new Forecast(items));
+      weatherCache[searchQuery]=weatherArr;
+      res.status(200).send(weatherArr);
+      }catch(error){
+      
+      handleError(error,res);
+      }
     }
+   
     };
 
     class Forecast{
