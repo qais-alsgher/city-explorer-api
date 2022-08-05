f41ba9aec7b0d76ed32e8f995d11fd8f13d24e00
qais-alsgher <qaisalsgher@gmail.com>
2022-08-05 15:29:11 +0300

    git movies and weather form api and send res for front

--- server.js
+++ server.js
@@ -13,15 +13,16 @@ const port = process.env.PORT || 3001;
 
 const weatherData= require('./data/weather.json');
 
-app.get('/weather',(req,res)=>{
+app.get('/weather',async(req,res)=>{
+let lat =req.query.lat;
+let lon=req.query.lon;
 
-let searchQuery=req.query.searchQuery;
-
-const city = weatherData.find(ele=> ele.city_name.toLowerCase() === searchQuery.toLowerCase());
-// console.log(city);
+// const city = weatherData.find(ele=> ele.city_name.toLowerCase() === searchQuery.toLowerCase());
+const city =await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
+// console.log(city.data);
 try{
-const weatherArr = city.data.map(items =>new Forecast(items));
-console.log(weatherArr);
+const weatherArr = city.data.data.map(items =>new Forecast(items));
+// console.log(weatherArr);
 res.send(weatherArr);
 }catch(error){
 
@@ -30,8 +31,45 @@ handleError(error,res);
 }
 );
 
+
+app.get('/movies',async(req,res)=>{
+
+let searchQuery=req.query.searchQuery;
+
+const movieArr =await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`);
+// console.log(movieArr.data.title);
+
+
+try{
+ const cityMovies=movieArr.data.results.map(items =>new Movies(items)); 
+// console.log(cityMovies);
+res.send(cityMovies);
+
+}catch(error){
+
+  handleError(error,res);
+}
+
+});
+
+
+class Movies{
+constructor(movie){
+
+  this.title = movie.title;
+  this.overview =movie.overview;
+  this.average_votes = movie.vote_average;
+  this.total_votes = movie.vote_count;
+  this.image_url = movie.poster_path;
+  this.popularity = movie.popularity;
+  this.release_date = movie.release_date;
+
+}
+};
+
+
 function handleError(error,res){
-res.status(500).send("the city dose not have data for weather for server");
+res.status(500).send("Error in Data Base");
 
 }
 
@@ -41,7 +79,6 @@ this.description= day.weather.description;
 this.date= day.valid_date;
   }
 }
-
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`)
